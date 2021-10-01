44a4a5ed84e670e5cc2595e55c9f5ed0b20fc173
asiaromaria <designsbyarc@gmail.com>
2021-09-30 18:23:48 -0700

    added hooks for axios call

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,61 +1,174 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
+const Register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [dob, setDob] = useState("");
+    const [ssn, setSsn] = useState("");
 
-class Register extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-        console.log(this.handleSubmit);
-      }
-    render() { 
-        return (  
-            <div>
-                Sign up and start saving and building credit today!
-            
-            <form>
+    const handleSubmit = async () => {
+        let data = {
+            name: name,
+            email: email,
+            password: password,
+            dob: dob,
+            ssn: ssn,
+        };
+        try {
+            let response = await axios.post("http://localhost:2000/api/accounts/", data);
+            localStorage.setItem("token", response.headers["x-auth-token"]);
+            // window.location = "/";
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return ( 
+        
+        <form onSubmit={handleSubmit}>
             <label>
-              First Name:
-              <input type="text" name="first name" />
+                Full Name:
+                <input
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
             </label>
             <label>
-              Last Name:
-              <input type="text" name="last name" />
+                Email:
+                <input
+                    type="text"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
             </label>
             <label>
-              Email:
-              <input type="text" name="email" />
+                Password:
+                <input
+                    type="text"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
             </label>
             <label>
-              Password:
-              <input type="text" name="password" />
+                DOB:
+                <input
+                    type="text"
+                    name="dob"
+                    value={dob}
+                    onChange={(e) => setDob(e.target.value)}
+                    />
             </label>
             <label>
-              Date of Birth:
-              <input type="text" name="birthday" />
-            </label>
-            <label>
-              Social Security Number:
-              <input type="text" name="password" />
+                SSN:
+                <input
+                    type="text"
+                    name="ssn"
+                    value={ssn}
+                    onChange={(e) => setSsn(e.target.value)}
+                    />
             </label>
             <input type="submit" value="Submit" />
-          </form>
-          </div>
-        );
-    }
+        </form>
+     );
 }
  
 export default Register;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Register(props) {
+//   const username = useFormInput('');
+//   const password = useFormInput('');
+//   const fullname = useFormInput('');
+//   const email = useFormInput('');
+//   const dob = useFormInput('');
+//   const ssn = useFormInput('');
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(false);
+ 
+//   // handle button click of Register form
+//   const handleRegister = () => {
+//     props.history.push('/dashboard');
+//   }
+ 
+//   return (
+//     <div>
+//       Register<br /><br />
+//       <div>
+//         Username:<br />
+//         <input type="text" {...username} autoComplete="new-password" />
+//       </div>
+//       <div>
+//         Full Name:<br />
+//         <input type="text" {...fullname} autoComplete="new-password" />
+//       </div>
+//       <div>
+//         Email:<br />
+//         <input type="text" {...email} autoComplete="new-password" />
+//       </div>
+//       <div>
+//         Date of Birth:<br />
+//         <input type="text" {...dob} autoComplete="new-password" />
+//       </div>
+//       <div>
+//         SSN:<br />
+//         <input type="text" {...ssn} autoComplete="new-password" />
+//       </div>
+//       <div style={{ marginTop: 10 }}>
+//         Password<br />
+//         <input type="password" {...password} autoComplete="new-password" />
+//       </div>
+//       {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+//       <input type="button" value={loading ? 'Loading...' : 'Register'} onClick={handleRegister} disabled={loading} /><br />
+//     </div>
+//   );
+// }
+ 
+// const useFormInput = initialValue => {
+//   const [value, setValue] = useState(initialValue);
+ 
+//   const handleChange = e => {
+//     setValue(e.target.value);
+//   }
+//   return {
+//     value,
+//     onChange: handleChange
+//   }
+// }
+ 
+ 
 
 

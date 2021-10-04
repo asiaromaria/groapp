6271170580febcd04f7c4610dace2ea253383c25
asiaromaria <designsbyarc@gmail.com>
2021-10-04 13:28:19 -0700

    fixed routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
 import PreLogin from '../Home/PreLogin';
@@ -6,11 +6,25 @@ import Savings from '../Savings/Savings';
 import CreditHistory from '../CreditHistory/CreditHistory';
 import Dashboard from '../Dashboard/Dashboard';
 import NotFound from '../NotFound/NotFound';
+import { BrowserRouter as Router, Route, NavLink, Switch, Redirect } from "react-router-dom";
+import jwtDecode from 'jwt-decode';
 
 
-import { BrowserRouter as Router, Route, NavLink, Switch } from "react-router-dom";
+
 
 function App () {
+    const [user, setUser] = useState(null);
+
+
+    useEffect(() => {
+      const jwt = localStorage.getItem("token");
+  
+      try {
+        const decoded = jwtDecode(jwt);
+        setUser(decoded);
+      } catch {}
+    }, []); 
+
     return (
         <Router>
         <div className="App">
@@ -25,11 +39,21 @@ function App () {
 
             <Switch>
             <Route exact path="/home" component={PreLogin}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
+            <Route 
+            path="/login" 
+            render={props => {
+                if (!user) {
+                    return <Redirect to="/register" />;
+                } else {
+                    return <Dashboard {...props} user={user} />
+                }
+            }} component={Login}
+            />
+            <Route path="/dashboard" component={Dashboard}/>
             <Route path="/savings" component={Savings}/>
             <Route path="/credithistory" component={CreditHistory}/>
-            <Route path="/dashboard" component={Dashboard}/>
+            <Route path="/register" 
+            component={Register}/>
             <Route component={NotFound} />
             </Switch>
         </div>

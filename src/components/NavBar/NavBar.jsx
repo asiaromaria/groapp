f18d976cfe278b345dff68429d437a003258c550
asiaromaria <designsbyarc@gmail.com>
2021-10-26 17:07:52 -0700

    added navbar for conditional rendering.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,48 @@
-import { Link } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import axios from "axios";
 
-class NavBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <nav>
-                <ul>
-                    <Link to='/'>
-                        <li>Home</li>
-                    </Link>
-                    <Link to='/Register'>
-                        <li>Register</li>
-                    </Link>
-                    <Link to='/Login'>
-                        <li>Login</li>
-                    </Link>
-                    <Link to='/Savings'>
-                        <li>Savings</li>
-                    </Link>
-                    <Link to='/Credithistory'>
-                        <li>Credit History</li>
-                    </Link>
-                </ul>
-            </nav>
-         );
-    }
-}
- 
+const NavBar = ({ user, logoutUser }) => {
+
+
+
+  return (
+    <div>
+      {user ? (
+        <ul>
+          {/* <h4>Welcome {user.username}</h4> */}
+          <h4>Welcome {user.name}</h4>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/savings">Savings</Link>
+          </li>
+          <li>
+            <Link to="/credithistory">Credit History</Link>
+          </li>
+          <li>
+            <Link to="/account">Account</Link>
+          </li>
+          <li>
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li>
+            <Link to="/" onSubmit={logoutUser}>Logout</Link>
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
 export default NavBar;

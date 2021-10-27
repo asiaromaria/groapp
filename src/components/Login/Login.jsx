54a51b1fb2423636c434876064ff44f014ab44ab
asiaromaria <designsbyarc@gmail.com>
2021-10-26 17:08:09 -0700

    trying to log in user with token

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,25 +3,32 @@ import './Login.css'
 import axios from 'axios';
 import { Form } from 'react-bootstrap';
 
-function Login() {
+const Login = () => {
 
-
+    const [user, setUser] = useState({})
     const [email,setEmail]= useState("");
     const [password,setPassword]= useState("");
 
     const submitHandler =(e) =>{
         e.preventDefault();
+
+       
     };
 
     const loginUser = async () => {
-        await axios.post('http://localhost:2000/api/auth',{
+      // const data = {
+      //   email: email,
+      //   password: password
+      // }
+        await axios.post('http://localhost:2000/api/auth', {
+          email: email,
+          password: password,
 
-            email: email,
-            password: password,
         })
         .then((res) => {
-            console.log(res)
-            localStorage.setItem('token', res.data);
+            setUser(res)
+            console.log( "hello user", user )
+            localStorage.setItem('token', res.token);
             window.location = '/dashboard';
         })
         .catch(error => console.log(error))
@@ -29,9 +36,10 @@ function Login() {
 
 
     return (
+
       <div className="Login">
         <h2>login</h2>
-        <form onSubmit={submitHandler} className="Login-form">
+        <form onSubmit={submitHandler} className="Login-form" onClick={() => loginUser()}>
           <Form.Group size="md" controlId="small">
             <Form.Label> Email: </Form.Label>
             <Form.Control
@@ -40,6 +48,7 @@ function Login() {
               value={email}
               placeholder="Email..."
               onChange={(e) => setEmail(e.target.value)}
+              onClick={() => loginUser()}
             />
           </Form.Group>
           <Form.Group size="md" controlId="small">
@@ -50,9 +59,11 @@ function Login() {
               value={password}
               placeholder="Password"
               onChange={(e) => setPassword(e.target.value)}
+              onClick={() => loginUser()}
+
             />
           </Form.Group>
-            <button onClick={() => {loginUser();}}>Login </button>
+            <button onClick={() => loginUser()}>Login </button>
         </form>
       </div>
     );
